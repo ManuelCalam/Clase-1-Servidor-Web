9d4f687a725168300fb6740ba5ddcd2a4fe23f6b
ManuelCalam <jmanuelc369@gmail.com>
2024-02-12 23:16:27 -0600

    Segundo commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+
 let Album = [
-    {'id': 1, 'nombre': "Album 1"},
-    {'id': 2, 'nombre': "Album 2"},
-    {'id': 3, 'nombre': "Album 3"}
+    {'id': 1, 'nombre': "Graduation"},
+    {'id': 2, 'nombre': "DAMN."},
+    {'id': 3, 'nombre': "Donda"},
+    {'id': 4, 'nombre': "Rodeo"}
+
 
 ]
 
@@ -18,9 +23,41 @@ app.get('/', function(req, res){
     res.send(Album)
 })
 
-app.post('/Album', function(req, res){
-    let Nuevo = [{
-        'id': 4, 'nombre': "Album 4"   }]
-    Album.push(Nuevo)
-    res.send('Usuario agregado')
+app.post('/PostAlbum', (req, res) =>{
+    let id = req.body.id 
+    let nombre = req.body.nombre
+    Album.push({id, nombre})
+    res.send(Album)
 })
+
+app.patch('/PatchAlbum/:id', function(req, res){
+    let newInfo = req.body
+    let id_patch = req.params.id
+
+    let infoActualizar = Album.find(Referencia => Referencia.id == id_patch)
+
+    Object.assign(infoActualizar, newInfo)
+
+    res.send(Album)
+})
+
+app.delete('/DeleteAlbum/:id', function(req, res){
+    let id_delete = req.params.id
+    let indexEliminar = Album.findIndex(Referencia => Referencia.id == id_delete)
+
+    Album.splice(indexEliminar, 1)
+
+    res.send(Album)
+})
+
+
+app.get('/Busqueda/:id', function(req, res){
+    let idBusqueda = req.params.id
+    let info = Album.find(Referencia => Referencia.id == idBusqueda)
+
+    res.send(info)
+
+})
+
+
+
